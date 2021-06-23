cc6ece571f3a138147122fb588e86440511bc994
angelatto <pudri109@gmail.com>
2021-06-23 14:37:03 +0900

    [update] 예약내역 클릭 시 모달 구현

--- src/views/Reservation/ReserveUpdateForm.js
+++ src/views/Reservation/ReserveUpdateForm.js
@@ -1,4 +1,5 @@
 import {React, useState} from 'react';
+import Modal from "react-modal";
 import classNames from 'classnames/bind';
 import style from './style.module.css';
 
@@ -11,6 +12,20 @@ import {
 import Button from 'views/common/Button';
 
 const cx = classNames.bind(style);
+const customStyles = {
+  content: {
+      width: '750px',
+      height: '430px',
+      top: '50%',
+      left: '44%',
+      right: 'auto',
+      bottom: 'auto',
+      marginRight: '-50%',
+      transform: 'translate(-50%, -50%)',
+  },
+};
+
+Modal.setAppElement('body');
 
 const ReserveUpdateForm = (props) => {
   // state 
@@ -27,46 +42,52 @@ const ReserveUpdateForm = (props) => {
   // };
 
   return (
-    <div className={cx("right-component-top")}>
-      <div className={cx("form-subject")}>
-        상세 예약
-      </div>
-      <div className={cx("reserve-form")}>
-        <Form
-          name="testForm"
-          onSubmit={data => {
-            // data[form element name]
-            setValues(data)
-            // do something with values
-          }}
-        >
-          <div>
-            <div className="float-right">
-                {/* <Button type="button" className={cx("ml-3", "custom-btn")}
-                  >방문 확인</Button>
-                <span className="ml-2 ">기존/신규</span> */}
-            </div>
-            <div>
-              <span className="">이름</span>
-              <TextBox className="mb-2" label="" name="myTextBox" />
-              <span>휴대전화</span>
-              <Telephone className="mb-2" label="" name="myTelephone" />
-            </div>
-          </div>
+    <Modal
+        isOpen={props.modalIsOpen}
+        onRequestClose={props.closeModal}
+        contentLabel="Reserve-Detail Modal2222"
+        style={customStyles}
+    >
+      <h5 className="ml-3 mb-1">예약 상세 내역</h5>
 
-          <span className="">예약 날짜</span>
-          <DateTime className="mb-2" label="" name="myDate" />
-          <span className="">예약 시간</span>
-          <DateTime className="mb-4" label="" type="time" name="myTime" />
-          <Button type="submit" className={cx("custom-btn")}
-          >예약 저장</Button>
-          <Button type="submit" className={cx("custom-btn")}
-          >영구 삭제</Button>
-          <Button type="submit" className={cx("custom-btn")}
-          >SMS 전송</Button>
-        </Form>
+      <div className={cx("reserve-form")}>
+          <Form
+            name="testForm"
+            onSubmit={data => {
+              setValues(data)
+            }}
+          >
+            <div>
+              <div className="float-right">
+                  {/* <Button type="button" className={cx("ml-3", "custom-btn")}
+                    >방문 확인</Button>
+                  <span className="ml-2 ">기존/신규</span> */}
+              </div>
+              <div>
+                <span className="">이름</span>
+                <TextBox className="mb-2" label="" name="myTextBox" />
+                <span>휴대전화</span>
+                <Telephone className="mb-2" label="" name="myTelephone" />
+              </div>
+            </div>
+
+            <span className="">예약 날짜</span>
+            <DateTime className="mb-2" label="" name="myDate" />
+            <span className="">예약 시간</span>
+            <DateTime className="mb-4" label="" type="time" name="myTime" />
+            <Button type="submit" className={cx("custom-btn")}
+            >예약 저장</Button>
+            <Button type="button" className={cx("custom-btn")}
+            >영구 삭제</Button>
+            <Button type="button" className={cx("custom-btn")}
+            >SMS 전송</Button>
+
+
+            <Button type="button" className={cx("custom-btn")} onClick={props.closeModal}
+            >닫기</Button>
+          </Form>
       </div>
-    </div>
+    </Modal>
   )
 };
 
--- src/views/Reservation/index.js
+++ src/views/Reservation/index.js
@@ -2,9 +2,7 @@ import React, { useState } from 'react';
 import style from './style.module.css';
 import classNames from 'classnames/bind';
 import ReserveCreateForm from './ReserveCreateForm';
-import ReserveUpdateForm from './ReserveUpdateForm';
-import ReserveDetail from './ReserveDetail';
-import DayzTestComponent from './DaysTestComponent';
+import ReserveCalendar from './ReserveCalendar';
 import Header from 'views/common/Header';
 import DialMenu from 'views/common/DialMenu';
 
@@ -12,12 +10,6 @@ const cx = classNames.bind(style);
 
 const Reservation = (props) => {
   // state
-  const [modalIsOpen, setIsOpen] = useState(false);
-
-  // 신규 등록 모달 
-  function openModal() { setIsOpen(true); }
-  function closeModal() { setIsOpen(false); }
-
 
   return (
     <>
@@ -26,19 +18,16 @@ const Reservation = (props) => {
         {/* 좌측  */}
         <div className={cx("left-component")}>
           {/* 1. 예약 결과 조회 컴포넌트 */}
-          <DayzTestComponent/>
-          {/* 2. 모달 창 - 예약 정보 R,U,D  */}
-          <ReserveDetail modalIsOpen={modalIsOpen} closeModal={closeModal}/>
+          <ReserveCalendar/>
         </div>
         
         {/* 우측 */}
         <div className={cx("right-component")}>
           <div>
-            {/* 3. 예약 Create 컴포넌트 
-                       Update 컴포넌트가 모드에 따라서 다른 것이 나옴 */}
+            {/* 2. 예약 Create 컴포넌트 */}
             <ReserveCreateForm/>
-            <ReserveUpdateForm/>
-            {/* 4. 예약 불가능 시간대 달력 커스터마이징 */}
+
+            {/* 3. 예약 불가능 시간대 달력 커스터마이징 */}
             {/* <CalendarCustomize/> */}
           </div>
         </div>
