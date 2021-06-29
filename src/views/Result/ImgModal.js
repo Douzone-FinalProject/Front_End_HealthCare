856e69ad770ff099a505c99f199432d8d68fa091
gangbyeongju <gangbungjoo@naver.com>
2021-06-29 10:20:33 +0900

    [update] result 0629

--- src/views/Result/ImgModal.js
+++ src/views/Result/ImgModal.js
@@ -1,10 +1,12 @@
 import Modal from "react-modal";
+import "react-image-gallery/styles/scss/image-gallery.scss";
+import ImageGallery from "react-image-gallery";
 
 //신규 환자 생성 modal 스타일 설정
 const customStyles = {
     content: {
-        width: '600px',
-        height: '700px',
+        width: '650px',
+        height: '870px',
         top: '50%',
         left: '50%',
         right: 'auto',
@@ -16,6 +18,28 @@ const customStyles = {
 
 Modal.setAppElement('body');
 function ImgModal(props) {
+    const images = [
+        {
+            original: 'http://localhost:3000/mri1.jpg',
+            thumbnail: 'http://localhost:3000/mri1.jpg',
+            originalHeight: '700px',
+        },
+        {
+            original: 'http://localhost:3000/mri1.jpg',
+            thumbnail: 'http://localhost:3000/mri1.jpg',
+            originalHeight: '700px',
+        },
+        {
+            original: 'http://localhost:3000/mri2.png',
+            thumbnail: 'http://localhost:3000/mri2.png',
+            originalHeight: '700px',
+        },
+        {
+            original: 'http://localhost:3000/mri2.png',
+            thumbnail: 'http://localhost:3000/mri2.png',
+            originalHeight: '700px',
+        },
+    ];
 
     return (
         <Modal
@@ -24,7 +48,7 @@ function ImgModal(props) {
             contentLabel="Create Patient Modal"
             style={customStyles}
         >
-            <img src="http://localhost:3000/mri1.jpg" alt="" height="100%"/>
+            <ImageGallery items={images} thumbnailPosition="top" startIndex={0} showBullets={true}/>
         </Modal>
     );
 }
