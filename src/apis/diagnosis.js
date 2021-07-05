6de9b0e0207278dfbd17e20419b3bb650940c8b0
dohee479 <dohee3959@gmail.com>
2021-07-05 16:28:55 +0900

    [updata]mediceModal

--- src/apis/diagnosis.js
+++ src/apis/diagnosis.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 // 약품검색 
-export function searchMedicine(word) {
-    const promise = axios.get(`/auth/login/${word}`);
-    return promise;
+export async function searchMedicine(keyword) {
+  const medicines = await axios.get(`/diagnostic/medicine?keyword=${keyword}`);
+  console.log(medicines.data)
+  return medicines.data
 }
