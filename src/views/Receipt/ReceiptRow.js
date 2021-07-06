6651ba54a68f09f2cfc7e90547d5dde7449d7c99
angelatto <pudri109@gmail.com>
2021-07-06 10:25:28 +0900

    [update] receipt_state color UI

--- src/views/Receipt/ReceiptRow.js
+++ src/views/Receipt/ReceiptRow.js
@@ -20,12 +20,11 @@ const ReceiptRow = (props) => {
         <span style={{width:"230px"}}>{props.receipt.patient_sex}</span>
         <span style={{width:"220px"}}>{props.receipt.patient_phone}</span>
         <span style={{width:"250px"}}>
-          {
-            props.receipt.receipt_state === '대기'?
-            <span style={{color:"orange"}}>{props.receipt.receipt_state}</span>
-            :
-            <span style={{color:"red"}}>{props.receipt.receipt_state}</span>
-          }
+          {props.receipt.receipt_state === '대기' && <span style={{color:"orange"}}>{props.receipt.receipt_state}</span>}
+          {props.receipt.receipt_state === '진료중' && <span style={{color:"red"}}>{props.receipt.receipt_state}</span>}
+          {props.receipt.receipt_state === '검사중' && <span style={{color:"#3BC9DB"}}>{props.receipt.receipt_state}</span>}
+          {props.receipt.receipt_state === '수납전' && <span style={{color:"37b24d"}}>{props.receipt.receipt_state}</span>}
+          {props.receipt.receipt_state === '완료' && <span style={{color:"black"}}>{props.receipt.receipt_state}</span>}
         </span>
         <span style={{width:"250px"}}>{props.receipt.receipt_datetime}</span>
     </div>
