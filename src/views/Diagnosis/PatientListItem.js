a48b610c9a18efa2f22a15624e792dbe23c97477
Jomsang <af7078@naver.com>
2021-07-06 09:01:34 +0900

    [update] 진료 검사요청 중

--- src/views/Diagnosis/PatientListItem.js
+++ src/views/Diagnosis/PatientListItem.js
@@ -1,17 +1,17 @@
 function PatientListItem(props) {
 
-    const selectPatient = (patient_id, patient_name, receipt_state) => {
-        props.selectPatient(patient_id, patient_name, receipt_state);
+    const selectPatient = (patient_id, patient_name, receipt_state, receipt_id) => {
+        props.selectPatient(patient_id, patient_name, receipt_state, receipt_id);
     };
     
     return(
-                <tr key={props.patient.patient_id} onClick={() => {selectPatient(props.patient.patient_id, props.patient.patient_name, props.patient.receipt_state)}}>
+                <tr key={props.patient.patient_id} onClick={() => {selectPatient(props.patient.patient_id, props.patient.patient_name, props.patient.receipt_state, props.patient.receipt_id)}}>
                     <td>{props.patient.patient_id}</td>
                     <td>{props.patient.patient_name}</td>
-                    {props.patient.receipt_state === "진료 중" && <td style={{color:"red"}}>{props.patient.receipt_state}</td>}
-                    {props.patient.receipt_state === "수납 전" && <td style={{color:"blue"}}>{props.patient.receipt_state}</td>}  
+                    {props.patient.receipt_state === "진료중" && <td style={{color:"red"}}>{props.patient.receipt_state}</td>}
+                    {props.patient.receipt_state === "수납전" && <td style={{color:"blue"}}>{props.patient.receipt_state}</td>}  
                     {props.patient.receipt_state === "대기" && <td style={{color:"gold"}}>{props.patient.receipt_state}</td>}
-                    {props.patient.receipt_state === "검사 중" && <td style={{color:"#3bc9db"}}>{props.patient.receipt_state}</td>}
+                    {props.patient.receipt_state === "검사중" && <td style={{color:"#3bc9db"}}>{props.patient.receipt_state}</td>}
                     <td>{props.patient.receipt_datetime}</td> 
                 </tr>
     );
