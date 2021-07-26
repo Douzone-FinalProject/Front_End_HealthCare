ca72deee1dc84e10800de29db892c5d7673eb097
Jomsang <af7078@naver.com>
2021-07-26 20:49:15 +0900

    [update] 지료 최종 수정 완료

--- src/apis/receipt.js
+++ src/apis/receipt.js
@@ -1,48 +1,48 @@
 import axios from "axios";
 
 export function getPatientList() {
-  return axios.get("/api/receipt/patients");
+  return axios.get("/receipt/patients");
 }
 
 // 환자 이름 검색 
 export function getPatientListByName(patient_name) {
-  return axios.get("/api/receipt/patients/name/"+patient_name);
+  return axios.get("/receipt/patients/name/"+patient_name);
 }
 
 export function getPatientById(patient_id) {
-  return axios.get("/api/receipt/patient/"+patient_id);
+  return axios.get("/receipt/patient/"+patient_id);
 }
 
 export function addNewPatient(patient) {
-  const promise = axios.post("/api/receipt/addPatient", patient);  
+  const promise = axios.post("/receipt/addPatient", patient);  
   return promise;
 }
 
 export function deletePatientById(patient_id) {
-  return axios.delete("/api/receipt/patient/"+patient_id);
+  return axios.delete("/receipt/patient/"+patient_id);
 }
 
 export function updatePatient(patient) {
-  return axios.put("/api/receipt/patient", patient);
+  return axios.put("/receipt/patient", patient);
 }
 
 // 접수 추가 
 export function insertReceipt(receipt) {
   console.log('7787 receipt: ', receipt);
-  return axios.post("/api/receipt", receipt);
+  return axios.post("/receipt", receipt);
 }
 
 // 접수 리스트 불러오기 
 export function getReceiptList() {
-  return axios.get("/api/receipt");
+  return axios.get("/receipt");
 }
 
 // 접수 취소 
 export function deleteReceiptById(receipt_id) {
-  return axios.delete("/api/receipt/"+receipt_id);
+  return axios.delete("/receipt/"+receipt_id);
 }
 
 // receipt_state 변경 
 export function updateReceipt(receipt_id, nextState) {
-  return axios.put("/api/receipt/"+receipt_id+"/"+nextState);
+  return axios.put("/receipt/"+receipt_id+"/"+nextState);
 }
